5b7a822edd3b04ea7caf380ee1814e2e5be3b1bc
Marcin Maciukiewicz <mm@bluepocket.com>
2013-12-31 17:50:46 +0000

    Draft version: Refactoring, create common structure for future commands. Use single binary to launch commands: ios_tool

--- lib/ios_dev_tools.rb
+++ lib/ios_dev_tools.rb
@@ -2,8 +2,13 @@
 module IOSDevTools
 
   autoload :VERSION,              'ios_dev_tools/version'
-  autoload :ApplicationBundle,    'ios_dev_tools/application_bundle'
-  autoload :ProvisioningProfile,  'ios_dev_tools/provisioning_profile'
-  autoload :InfoPlist,            'ios_dev_tools/info_plist'
+  autoload :ApplicationBundle,    'ios_dev_tools/model/application_bundle'
+  autoload :ProvisioningProfile,  'ios_dev_tools/model/provisioning_profile'
+  autoload :InfoPlist,            'ios_dev_tools/model/info_plist'
+  autoload :Pack,                 'ios_dev_tools/commands/pack'
+  autoload :Sign,                 'ios_dev_tools/commands/sign'
+  autoload :Verify,               'ios_dev_tools/commands/verify'
+  autoload :Tool,                 'ios_dev_tools/commands/tool'
+  autoload :Help,                 'ios_dev_tools/commands/help'
 
 end
---
+++ lib/ios_dev_tools/commands/verify.rb
@@ -0,0 +1,15 @@
+
+module IOSDevTools
+
+  class Verify
+
+    def self.create_with_args cmd_line_arguments
+      Verify.new
+    end
+
+    def execute
+      puts "TODO: execute"
+    end
+  end
+
+end

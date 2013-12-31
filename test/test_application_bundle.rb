0f7a75dd4e33167cf4b6b33e8cabe2b9f956fd84
Marcin Maciukiewicz <mm@bluepocket.com>
2013-12-31 16:49:20 +0000

    Extract info.plist related methods from Application bundle and encapsulate in InfoPlist class

--- test/test_application_bundle.rb
+++ test/test_application_bundle.rb
@@ -3,6 +3,18 @@ require 'ios_dev_tools'
 
 class ApplicationBundleTest < Test::Unit::TestCase
 
+  def test_create_with_app_folder
+
+      app_bundle=IOSDevTools::ApplicationBundle.new "test_resources/DummyApp.app"
+
+      assert_not_nil app_bundle
+      info_plist=app_bundle.info_plist
+      assert_not_nil info_plist
+      assert_equal "Dummy Bundle", app_bundle.info_plist.display_name
+      assert_equal "com.csquirrel.dummy.bundle", app_bundle.info_plist.bundle_id
+
+  end
+
   def test_create_with_not_existing_folder
 
     assert_raise RuntimeError do
---
+++ test/test_info_plist.rb
@@ -0,0 +1,23 @@
+require 'test/unit'
+require 'ios_dev_tools'
+
+class ApplicationBundleTest < Test::Unit::TestCase
+
+  def test_create_with_file
+
+      info_plist=IOSDevTools::InfoPlist.new "test_resources/DummyApp.app/Info.plist"
+
+      assert_equal "Dummy Bundle", info_plist.display_name
+      assert_equal "com.csquirrel.dummy.bundle", info_plist.bundle_id
+
+  end
+
+  def test_create_with_not_existing_file
+
+    assert_raise RuntimeError do
+      IOSDevTools::ApplicationBundle.new "dummy.file.plist"
+    end
+
+  end
+
+end
